f6752c5d32116cb07340f116c2f2e4ed06ea4e5f
Martin Suba <martin.suba01@gmail.com>
2020-04-01 18:26:24 +0200

    Implement fork child_process

--- fork/index.ts
+++ fork/index.ts
@@ -1,34 +1,57 @@
 import express from 'express';
-import { fork } from 'child_process';
+import { fork, ChildProcess } from 'child_process';
 import path from 'path';
 
 import ServerError from '../ServerError';
 
+const RES_TIMEOUT = 20000;
+
 const app = express();
 
 app.get('/api/t9/:number', (req, res) => {
-  const { number } = req.params;
-  const isNumber = /^\d+$/.test(number);
+  try {
+    let worker: ChildProcess;
+    const { number } = req.params;
+    const isNumber = /^\d+$/.test(number);
 
-  if (!isNumber) {
-    const err = new ServerError({
-      message: `Server error: '${number}' should be a number`,
-      code: 'invalid_input',
+    res.setTimeout(RES_TIMEOUT, () => {
+      const err = new ServerError({
+        message: `Server error: Response timeout: ${RES_TIMEOUT}`,
+        code: 'timeout',
+      });
+      res.send({
+        res: [],
+        err,
+      });
+
+      if (worker && !worker.killed) {
+        worker.kill();
+      }
     });
-    res.send({
-      res: [],
-      err,
+
+    if (!isNumber) {
+      const err = new ServerError({
+        message: `Server error: '${number}' should be a number`,
+        code: 'invalid_input',
+      });
+      res.send({
+        res: [],
+        err,
+      });
+      return;
+    }
+
+    worker = fork(path.resolve(__dirname, './worker'));
+    worker.on('message', ({ result }: Record<string, []>) => {
+      res.send({ res: result });
+
+      worker.kill();
     });
-    return;
+
+    worker.send({ number });
+  } catch (err) {
+    console.error(err);
   }
-
-  const worker = fork(path.resolve(__dirname, './worker'));
-  worker.on('message', ({ result }: Record<string, []>) => {
-    res.send({ res: result });
-    worker.kill();
-  });
-
-  worker.send({ number });
 });
 
 app.listen(8080, () => console.log(`Listening on port ${8080}!`));
